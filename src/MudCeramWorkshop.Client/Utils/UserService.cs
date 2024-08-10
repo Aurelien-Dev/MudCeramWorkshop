58e5d7b17928ed83749ec1f71997947b9dcc3839
Aurélien <a.cretaine@live.fr>
2024-08-10 15:52:26 +0200

    Ajout du status connected pour simplifier et vérification des claims avant d'obtenir la data

--- src/MudCeramWorkshop.Client/Utils/UserService.cs
+++ src/MudCeramWorkshop.Client/Utils/UserService.cs
@@ -9,17 +9,36 @@ namespace MudCeramWorkshop.Client.Utils
             var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
             var user = authState.User;
 
-            return new UserInfoState()
-            {
-                WorkshopName = user.FindFirst("WorkshopName").Value,
-                WorkshopId = Convert.ToInt16(user.FindFirst("WorkshopId").Value)
-            };
+            string? wName = user.FindFirst("WorkshopName")?.Value;
+            int? wId = user.FindFirst("WorkshopId") is null ? null : Convert.ToInt16(user.FindFirst("WorkshopId").Value);
+
+            return new UserInfoState(wName, wId, user.Identity.IsAuthenticated);
         }
     }
 
     public class UserInfoState
     {
-        public string WorkshopName { get; set; }
-        public int WorkshopId { get; set; }
+        private string? WorkshopName { get; set; }
+        private int? WorkshopId { get; set; }
+        public bool IsAuthenticated { get; internal set; }
+
+        public UserInfoState(string? workshopName, int? workshopId, bool isAuthenticated)
+        {
+            WorkshopName = workshopName;
+            WorkshopId = workshopId;
+            IsAuthenticated = isAuthenticated;
+        }
+
+        public string GetWorkshopName()
+        {
+            return WorkshopName ?? string.Empty;
+        }
+
+        public int GetWorkshopId()
+        {
+            if (WorkshopId == null) throw new ArgumentNullException(nameof(WorkshopId));
+
+            return (int)WorkshopId;
+        }
     }
 }
