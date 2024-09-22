9f038fc9786eadd58a409d385100629a478cd428
Aurélien C. <a.cretaine@live.fr>
2024-09-22 19:59:47 +0200

    Evol/chat gpt eshop (#2)
    * Chat GPT integration
    
    * Add Chat GPT Fonctionnality
    
    Enhance UI, features, and performance in workshops module
    
    - Added `SelectedImage` property to `AssociationImage.razor`.
    - Updated `CarouselIndexChanged` to set `SelectedImage`.
    - Removed `EshopGenerator` component and related code.
    - Updated `ProductDetailPage.razor` to use component references.
    - Replaced `LaunchGPT` handler with `OpenEshopGPT`.
    - Added `OpenEshopGPT` method to open e-shop description dialog.
    - Removed `EshopGenerator` reference from `ProductDetailPage.razor`.
    - Registered `ChatGptManager` service in `Program.cs`.
    - Added `DialogEshopGenerator.razor` component.
    - Created `ChatGptManager` class for OpenAI API interactions.
    - Added embedded resources for `DescriptionPrompt.txt` and `PersonatPrompt.txt`.
    - Updated project file to include new resources and remove old ones.
    
    * Refactor UI components and enhance ChatGPT integration
    
    Improved UI layout and structure across multiple Razor components:
    - MainLayout.razor: Added commented-out MudAppBar and note for pb-20.
    - FiringListPage.razor, MaterialListPage.razor, ProductListPage.razor: Updated MudContainer classes for better spacing.
    - AssociationFiring.razor: Added Class parameter to MudItem.
    - DialogEshopGenerator.razor: Replaced MudStack with MudAlert, MudGrid, and MudOverlay; added fields and properties for product details and loading state; updated dependency injections and added Submit method.
    - ProductDetailPage.razor: Refactored layout with MudStack and MudAlert; updated button actions and dialog options; added Typo field.
    
    Enhanced ChatGPT integration:
    - ChatGptManager.cs: Updated GenerateDescription to return a tuple with description and cost; added CalculateCost method; refactored GenerateChatMessage for readability.
    
    Adjusted typography:
    - CeramWorkshopTheme.cs: Changed H6 font size from 1.4rem to 1.2rem.
    
    * Sonarfix

---
+++ src/MudCeramWorkshop.Client/Managers/ChatGptManager.cs
@@ -0,0 +1,110 @@
+﻿using MudCeramWorkshop.Data.Domain.Models.MainDomain;
+using MudCeramWorkshop.Data.Domain.Models.MainDomain.Enums;
+using OpenAI_API;
+using OpenAI_API.Chat;
+using System.Reflection;
+
+namespace MudCeramWorkshop.Client.Managers
+{
+    public class ChatGptManager(IConfiguration Config)
+    {
+        public async Task<(string, decimal)> GenerateDescription(string imagePath, string materials, string tags, double diameter, double height)
+        {
+            if (string.IsNullOrWhiteSpace(imagePath)) { throw new ArgumentNullException(nameof(imagePath)); }
+
+            var api = new OpenAIAPI(Config["OpenAiSecret"]);
+
+            string imageBase64 = await DownloadImageAsBase64(imagePath);
+            string imageDataUrl = $"data:image/jpeg;base64,{imageBase64}";
+
+            ChatMessage personatPrompt = await GenerateChatMessage("PersonatPrompt", ChatMessageRole.Assistant);
+            ChatMessage instructionPrompt = await GenerateChatMessage("DescriptionPrompt", ChatMessageRole.User, imageDataUrl, (s) =>
+            {
+                return string.Format(s, tags, diameter, height, materials);
+            });
+
+            var result = await api.Chat.CreateChatCompletionAsync(
+                [
+                    personatPrompt,
+                    instructionPrompt
+                ], model: "gpt-4o-2024-08-06");
+
+            return (result.ToString(), CalculateCost(result));
+        }
+        private decimal CalculateCost(ChatResult result)
+        {
+            ////gpt-4o-mini
+            //// Tarifs en dollars par token (à ajuster selon les tarifs actuels)
+            //decimal costPerInputTokenUSD = 0.150m / 1000000m; // $0.150 / 1M input tokens
+            //decimal costPerOutputTokenUSD = 0.600m / 1000000m; // $0.600 / 1M output tokens
+
+            ////gpt-4o
+            //// Tarifs en dollars par token (à ajuster selon les tarifs actuels)
+            //decimal costPerInputTokenUSD = 5m / 1000000m; // $0.150 / 1M input tokens
+            //decimal costPerOutputTokenUSD = 15m / 1000000m; // $0.600 / 1M output tokens
+
+            //gpt-4o-2024-08-06
+            // Tarifs en dollars par token (à ajuster selon les tarifs actuels)
+            decimal costPerInputTokenUSD = 2.5m / 1000000m; // $0.150 / 1M input tokens
+            decimal costPerOutputTokenUSD = 10m / 1000000m; // $0.600 / 1M output tokens
+
+            // Taux de conversion USD -> EUR
+            decimal conversionRate = 0.85m;
+
+            // Obtenir le nombre de tokens utilisés
+            int inputTokens = result.Usage.PromptTokens;
+            int outputTokens = result.Usage.CompletionTokens;
+
+            // Calculer le coût total en dollars
+            decimal totalCostUSD = (inputTokens * costPerInputTokenUSD) + (outputTokens * costPerOutputTokenUSD);
+
+            // Convertir le coût en euros
+            decimal totalCostEUR = totalCostUSD * conversionRate;
+
+            return totalCostEUR;
+        }
+
+        public async Task<ChatMessage> GenerateChatMessage(string promptName, ChatMessageRole chatMessageRole, string imageDataUrl = null, Func<string, string> textFormaterAction = null!)
+        {
+            string assistantPrompt = await ReadEmbeddedResourceAsync($"MudCeramWorkshop.Client.Managers.Prompts.{promptName}.txt");
+            ChatMessage chatMessage = new ChatMessage(chatMessageRole, string.Empty);
+
+            if (textFormaterAction == null)
+                chatMessage.TextContent = assistantPrompt;
+            else
+                chatMessage.TextContent = textFormaterAction(assistantPrompt);
+
+            if (!string.IsNullOrWhiteSpace(imageDataUrl))
+                chatMessage.Images.Add(new ChatMessage.ImageInput(imageDataUrl));
+
+            return chatMessage;
+        }
+
+        private async Task<string> ReadEmbeddedResourceAsync(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                {
+                    throw new FileNotFoundException("Resource not found", resourceName);
+                }
+
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return await reader.ReadToEndAsync();
+                }
+            }
+        }
+
+        private async Task<string> DownloadImageAsBase64(string imageUrl)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                byte[] imageByte = await client.GetByteArrayAsync(imageUrl);
+
+                return Convert.ToBase64String(imageByte);
+            }
+        }
+    }
+}
